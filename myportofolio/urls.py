8d931f74a832e2685606dbeba31430b2436f23bc
ayamedayoo <samuelkevin321123@gmail.com>
2026-09-03 00:14:17 +0700

    sss

--- myportofolio/urls.py
+++ myportofolio/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from myportofolio import views  # Mengimpor file views dari folder utama
+from myportofolio import views  # Pastikan mengarah ke file views yang benar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.landing_page, name='landing_page'),  # Mengarahkan rute utama ke fungsi landing_page
+    path('', views.landing_page, name='landing_page'),  # Jalur utama ('') wajib ada
 ]
